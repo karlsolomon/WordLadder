cbcd921845dd69263d0ba11ec7fe75e8a5c8c2be
chrissickler <christopher.sickler@utexas.edu>
2016-09-28 11:27:35 -0500

    JUnit Test changes

--- assignment3/src/assignment3/SystemTest.java
+++ assignment3/src/assignment3/SystemTest.java
@@ -16,7 +16,7 @@ public class SystemTest {
 	}
 	
 	@Test
-	public void test() throws InterruptedException, ExecutionException {
+	public void testBFSvsBFS() throws InterruptedException, ExecutionException {
 		Long start;
 		Long end;
 		String time;
@@ -50,6 +50,58 @@ public class SystemTest {
 			
 			start = System.nanoTime();
 			tmp = Main.getWordLadderBFS(word2, word1);
+			end = System.nanoTime();
+			time = Long.toString(end-start);		
+			bfsSize = tmp.size();	
+			sizes[i][3] = String.valueOf(bfsSize);
+			sizes[i][5] = time;
+			bfsTime += (end-start);			
+			assertTrue(bfsSize == dfsSize);
+		}
+		System.out.println("Word1\tWord2\tDsize\tBsize\tDtime\tBtime");
+		for(int i = 0 ; i < numItems ; i ++) {
+			for(int j = 0 ; j < 6; j++) {
+				System.out.print(sizes[i][j] + '\t');
+			}
+			System.out.print("\n");
+		}		
+		System.out.println("\n\nAvg BFS Time: " + bfsTime/numItems + "\nAvg DFS Time: " + dfsTime/numItems);
+		
+	}
+	public void testDFSvsBFS() throws InterruptedException, ExecutionException {
+		Long start;
+		Long end;
+		String time;
+		ArrayList<String> tmp;
+		
+		
+		//RANDOM TESTING
+		int numItems = 1000;
+		String[][] sizes = new String[numItems][6];
+		Random r = new Random();
+		int bfsSize = 0;
+		int dfsSize = 0;
+		String word1;
+		String word2;
+		Long dfsTime = (long) 0;
+		Long bfsTime = (long) 0;
+		for(int i = 0 ; i < numItems; i ++) {
+			word1 = Words.dictionary.get(r.nextInt(Words.dictionary.size()));
+			word2 = Words.dictionary.get(r.nextInt(Words.dictionary.size()));
+			sizes[i][0] = word1;
+			sizes[i][1] = word2;
+			
+			start = System.nanoTime();
+			tmp = Main.getWordLadderDFS(word1, word2);
+			end = System.nanoTime();
+			time = Long.toString(end-start);
+			dfsSize = tmp.size();
+			sizes[i][2] = String.valueOf(dfsSize);
+			sizes[i][4] = time;
+			dfsTime += (end-start);
+			
+			start = System.nanoTime();
+			tmp = Main.getWordLadderBFS(word1, word2);
 			end = System.nanoTime();
 			time = Long.toString(end-start);		
 			bfsSize = tmp.size();	
